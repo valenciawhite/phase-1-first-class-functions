b0e5dd829bba9ecd0caf510d06f382d2171793c6
Valencia White <valenciawhite@rocketmail.com>
2022-02-24 08:10:44 -0500

    commit tree 9df9db53ee482b580bf0c2557622a3b7d6dd4b08

---
+++ index.js
@@ -0,0 +1,17 @@
+const sayHello = () => {
+    return "Hello";
+};
+
+const receivesAFunction = (greeter, person) => {
+      return greeter() + "" + person;
+  };
+  console.log(receivesAFunction(sayHello, "Annie"));
+
+  function returnsANamedFunction(foo) {
+    return
+  }
+       
+
+  console.log(returnsANamedFunction());
+
+ 
